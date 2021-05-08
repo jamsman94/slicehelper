26af999d67ecc5f4cb6e8a68bd30404821f35f84
Min Min <minmin@koderover.com>
2021-05-08 14:01:34 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# slicehelper
-A simple go tool to convert all nil slices to empty slices in you struct
+## slicehelper
+A simple go tool to make json Marshalling less painful for slices
+
+## install
+```
+go get -u github.com/jamsman94/slicehelper
+```
---
+++ slice.go
@@ -0,0 +1,92 @@
+package slicehelper
+
+import "reflect"
+
+func ReplaceNilWithEmptySlice(input interface{}) interface{} {
+	val := reflect.ValueOf(input)
+	switch val.Kind() {
+	case reflect.Ptr:
+		// if, in some case, a pointer is passed in, We dereference it and do the normal stuff
+		if val.IsNil() {
+			if reflect.TypeOf(input).Elem().Kind() == reflect.Slice {
+				res := reflect.New(reflect.TypeOf(input).Elem())
+				resp := reflect.MakeSlice(reflect.TypeOf(input).Elem(), 0,0)
+				res.Elem().Set(resp)
+				return res.Interface()
+			}
+			return input
+		}
+		return ReplaceNilWithEmptySlice(val.Elem().Interface())
+	case reflect.Slice:
+		res := reflect.New(val.Type())
+		resp := reflect.MakeSlice(val.Type(), 0, val.Len())
+		// if this is not empty, copy it
+		if !val.IsZero() {
+			// iterate over each element in slice
+			for i := 0; i < val.Len(); i++ {
+				item := val.Index(i)
+				var newItem reflect.Value
+				switch item.Kind() {
+				case reflect.Struct, reflect.Slice, reflect.Map:
+					// recursively handle nested struct
+					newItem = reflect.Indirect(reflect.ValueOf(ReplaceNilWithEmptySlice(item.Interface())))
+				case reflect.Ptr:
+					if item.IsNil() {
+						if item.Type().Elem().Kind() == reflect.Slice {
+							res := reflect.New(item.Type().Elem())
+							resp := reflect.MakeSlice(item.Type().Elem(), 0, 0)
+							res.Elem().Set(resp)
+							newItem = res
+							break
+						}
+						newItem = item
+						break
+					}
+					if item.Elem().Kind() == reflect.Struct || item.Elem().Kind() == reflect.Slice {
+						newItem = reflect.ValueOf(ReplaceNilWithEmptySlice(item.Elem().Interface()))
+						break
+					}
+					fallthrough
+				default:
+					newItem = item
+				}
+				resp = reflect.Append(resp, newItem)
+			}
+		}
+		res.Elem().Set(resp)
+		return res.Interface()
+	case reflect.Struct:
+		resp := reflect.New(reflect.TypeOf(input))
+		newVal := resp.Elem()
+		// iterate over input's fields
+		for i := 0; i < val.NumField(); i++ {
+			newValField := newVal.Field(i)
+			if !newValField.CanSet() {
+				continue
+			}
+			valField := val.Field(i)
+			if valField.Kind() == reflect.Ptr {
+				newValField.Set(reflect.ValueOf(ReplaceNilWithEmptySlice(valField.Interface())))
+			} else {
+				newValField.Set(reflect.Indirect(reflect.ValueOf(ReplaceNilWithEmptySlice(valField.Interface()))))
+			}
+		}
+		return resp.Interface()
+	case reflect.Map:
+		res := reflect.New(reflect.TypeOf(input))
+		resp := reflect.MakeMap(reflect.TypeOf(input))
+		// iterate over every key value pair in input map
+		iter := val.MapRange()
+		for iter.Next() {
+			k := iter.Key()
+			v := iter.Value()
+			// recursively handle nested value
+			newV := reflect.Indirect(reflect.ValueOf(ReplaceNilWithEmptySlice(v.Interface())))
+			resp.SetMapIndex(k, newV)
+		}
+		res.Elem().Set(resp)
+		return res.Interface()
+	default:
+		return input
+	}
+}
